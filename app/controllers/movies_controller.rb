dc0807d597fae61746f2c8594d4065cf3ba505bd
EC2 Default User <ec2-user@ip-172-31-41-13.us-east-2.compute.internal>
2019-09-30 23:54:14 +0000

    testing part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,8 +18,19 @@ class MoviesController < ApplicationController
       params[:sort_by] = session[:sort_by]
       redirect_to movies_path(params) and return
     end
+    
     @hilite = sort_by = session[:sort_by]
-    @movies = Movie.order(sort_by)
+    @all_ratings = Movie.all_ratings
+    
+    if params.key?(:ratings)
+      session[:ratings] = param[:ratings]
+    elsif session.key?(:ratings)
+      params[:ratings] = session[:ratings]
+      redirect_to movies_path(params) and return
+    end
+    
+    @checked_ratings = (session[:ratings].keys if session.key?(:ratings)) || @all_ratings
+    @movies = Movie.order(sort_by).where(rating: @checked_ratings)
   end
 
   def new
