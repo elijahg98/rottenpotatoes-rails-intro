29d2e9278ea70447985caa73ae66eb90e723857d
EC2 Default User <ec2-user@ip-172-31-41-13.us-east-2.compute.internal>
2019-10-01 02:23:41 +0000

    testing part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,26 +11,17 @@ class MoviesController < ApplicationController
   end
 
   def index
-    # @movies = Movie.all
-    if params.key?(:sort_by)
-      session[:sort_by] = params [:sort_by]
-    elsif session.key?(:sort_by)
-      params[:sort_by] = session[:sort_by]
-      redirect_to movies_path(params) and return
+    
+    @movies = Movie.all
+    
+    if session[:sort_method] == "title"
+      @movies = @movies.sort!{|a,b| a.title <=> b.title}
+      @movie_header = "hilite"
+    elsif session[:sort_method] == "release_date"
+      @movies = @movies.sort!{|a,b| a.release_date <=> b.release_date}
+      @date_header = "hilite"
     end
     
-    @hilite = sort_by = session[:sort_by]
-    @all_ratings = Movie.all_ratings
-    
-    if params.key?(:ratings)
-      session[:ratings] = param[:ratings]
-    elsif session.key?(:ratings)
-      params[:ratings] = session[:ratings]
-      redirect_to movies_path(params) and return
-    end
-    
-    @checked_ratings = (session[:ratings].keys if session.key?(:ratings)) || @all_ratings
-    @movies = Movie.order(sort_by).where(rating: @checked_ratings)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,3 @@
 class Movie < ActiveRecord::Base
-    @@all_ratings = ['G','PG','PG-13','R']
-
-	def self.all_ratings
-		@@all_ratings
-	end
+  
 end
